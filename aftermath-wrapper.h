55141426bca130413a519f03c366b1a465489832
Austin Shafer <ashafer@badland.io>
2022-06-17 22:52:34 -0400

    WIP: first building version

---
+++ aftermath-wrapper.h
@@ -0,0 +1,10 @@
+/*
+ * Austin Shafer - 2022
+ */
+
+#ifndef AFTERMATH_WRAPPER_H
+#define AFTERMATH_WRAPPER_H
+
+#include <GFSDK_Aftermath.h>
+
+#endif
---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+/// Nvidia Aftermath GPU crash dump utility
+///
+/// These are set of bindings for the Nvidia Aftermath SDK, which
+/// allows for GPU minidump generation.
+mod aftermath_sys;
+use aftermath_sys::*;
